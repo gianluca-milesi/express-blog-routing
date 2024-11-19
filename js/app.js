007f858ca6b95784b9a546eb82d36cfb20dbd96b
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-19 15:40:52 +0100

    ADD: all routes according to REST architecture

--- js/app.js
+++ js/app.js
@@ -6,16 +6,49 @@ const posts = require("./posts.js");
 app.use(express.static("public"));
 
 
-app.get("/", (req, res) => {
-    res.send("Server del mio blog");
+// app.get("/bacheca", (req, res) => {
+//     res.json({
+//         count: posts.length,
+//         AllPosts: posts
+//     });
+// });
+
+
+//Index
+app.get("/posts", (req, res) => {
+    res.send("Elenco delle pizze");
 });
 
-app.get("/bacheca", (req, res) => {
-    res.json({
-        count: posts.length,
-        AllPosts: posts
-    });
+//Show
+app.get("/posts/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Ecco la pizza con id: ${id}`);
 });
+
+//Store
+app.post("/posts", (req, res) => {
+    res.send("Creo una nuova pizza");
+});
+
+//Update
+app.put("/posts/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Aggiorno la pizza con id: ${id}`);
+});
+
+//Modify
+app.patch("/posts/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Modifico la pizza con id: ${id}`);
+});
+
+//Destroy
+app.delete("/posts/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Elimino la pizza con id: ${id}`);
+});
+
+
 
 
 app.listen(port, () => {
