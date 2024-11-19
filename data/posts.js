da71345a59e3c9a13a3cb72f9863614c28caf027
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-19 16:14:37 +0100

    ADD: routers file with routes for CRUD operations inside, exported to app.js

--- data/posts.js
+++ data/posts.js
@@ -1,29 +1,34 @@
 const posts = [
     {
+        id: 1,
         titolo: "Ciambellone",
         contenuto: "il ciambellone di mia nonna",
         url: "/images/ciambellone.jpeg",
         tags: ["ciambellone", "nonna", "cucina"]
     },
     {
+        id: 2,
         titolo: "Cracker Barbabietola",
         contenuto: "Strani ma buoni",
         url: "/images/cracker_barbabietola.jpeg.jpeg",
         tags: ["cracker", "barbabietola", "cucina"]
     },
     {
+        id: 3,
         titolo: "Pane Fritto",
         contenuto: "Dolcissimo",
         url: "/images/pane_fritto_dolce.jpeg",
         tags: ["pane", "fritto", "cucina"]
     },
     {
+        id: 4,
         titolo: "Pasta Barbabietola",
         contenuto: "Da assaggiare",
         url: "/images/pasta_barbabietola.jpeg",
         tags: ["pasta", "barbabietola", "cucina"]
     },
     {
+        id: 5,
         titolo: "Torta Paesana",
         contenuto: "Fatta in casa",
         url: "/images/torta_paesana.jpeg",
---
+++ routers/posts.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const posts = require("../data/posts.js");
+
+//Index
+router.get("/", (req, res) => {
+    // res.send("Elenco dei Posts");
+
+    res.json({
+        count: posts.length,
+        allPosts: posts
+    });
+});
+
+//Show
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    // res.send(`Ecco il post con id: ${id}`);
+    const post = posts.find((item) => item.id === id)
+    res.json(post);
+});
+
+//Store
+router.post("/", (req, res) => {
+    res.send("Creo un nuovo post");
+});
+
+//Update
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Aggiorno il post con id: ${id}`);
+});
+
+//Modify
+router.patch("/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Modifico il post con id: ${id}`);
+});
+
+//Destroy
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Elimino il post con id: ${id}`);
+});
+
+module.exports = router;
